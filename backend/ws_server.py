bea794b261fe5ae2c8a3b5c6db6c579b39c34445
jotalis <jayqwu@gmail.com>
2024-10-19 20:50:31 -0700

    feat: add classification text

--- backend/ws_server.py
+++ backend/ws_server.py
@@ -12,8 +12,9 @@ model = YOLO("./Experiments/runs/segment/train/weights/best.pt")
 PORT = 8080
 
 # Initialize video capture
-cap = cv2.VideoCapture(0)  # CHANGE THIS TO THE INPUT STREAM FOR THE HARDWARE
-
+cap = cv2.VideoCapture(1)  # CHANGE THIS TO THE INPUT STREAM FOR THE HARDWARE
+class_names = {0: 'forceps', 1: 'gauze', 2: 'scissors'}
+colors = [(255, 42, 4), (235, 219, 11), (243, 243, 243)]
 
 async def handle_connection(ws: websockets.WebSocketClientProtocol):
     print("Client connected")
@@ -25,21 +26,33 @@ async def handle_connection(ws: websockets.WebSocketClientProtocol):
         ret, frame = cap.read()
         if not ret:
             break
-
         resized_frame = cv2.resize(frame, (640, 480))
 
         # Get detection result
-        result = model(resized_frame, verbose=False)
-        results.append(result)
-
+        result = model(resized_frame, verbose=False, conf = 0.4)
         # Annotate the frame
-        annotated_frame = result[0].plot()
-
-        # cv2.imshow("frame", annotated_frame)
+        annotated_frame = result[0].plot(boxes = False)
+        
+        drawn = set()
+        for detection in result[0].boxes.data:
+            x1, y1, x2, y2, conf, cls = detection
+            cls = int(cls)
+            if cls in drawn:
+                continue
+            drawn.add(cls)
+            cv2.putText(
+                annotated_frame,
+                class_names[cls],
+                (int(x1), int(y1) - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.9,
+                colors[cls],
+                2,
+            )
 
         _, buffer = cv2.imencode(".jpg", annotated_frame)
         jpg_as_text = base64.b64encode(buffer).decode("utf-8")
-        await ws.send(jpg_as_text)
+        await ws.send("image:"+jpg_as_text)
 
         # Get the current time
         current_time = time.time()
@@ -54,24 +67,19 @@ async def handle_connection(ws: websockets.WebSocketClientProtocol):
                 # best_results.append((frame_idx, best_result))
                 res = best_result
                 # Assuming annotated_frame is the image you want to display
-                annotated_frame = res[0].plot()
                 metadata = res[0].boxes.cls
-                tensor_string = json.dumps(metadata.tolist())
-                print("Tensor String: ", tensor_string)
-                await ws.send(tensor_string)
+
+                # Convert the tensor to a Python list and then to a set of unique class indices
+                class_indices = set(metadata.tolist())
+
+                # Map the class indices to their corresponding class names
+                present_classes = [class_names[int(cls)] for cls in class_indices if int(cls) in class_names]
+                present_classes_string = json.dumps(present_classes)
+                await ws.send("metadata:"+present_classes_string)
 
             # Reset the time and clear the results for the next 5 seconds
             start_time = current_time
             results = []  # Clear results for the next window
-
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-    # Release the video capture and destroy windows
-    cap.release()
-    cv2.destroyAllWindows()
-
 
 def find_best_result(results):
     total_results = len(results)
